f897bddde730efbe66d6afd721e5ffaa1a050034
Alex Hover <ahover307@gmail.com>
2019-11-20 20:27:06 -0500

    debugging

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,8 @@ public class Main
 {
     private static final String defaultPassword = "";
 
-    private static String filePathForPassword = "Files\\Password.dat";
-    private static String filePathForAccounts = "Files\\Accounts.dat";
+    private static String filePathForPassword = "Files\\Password.txt";
+    private static String filePathForAccounts = "Files\\Accounts.txt";
     private static String filePathForIV = "Files\\IV.dat";
     private static String filePathForKey = "Files\\Key.dat";
 
@@ -121,9 +121,11 @@ public class Main
         byte[] saltBytes = Integer.toString(salt).getBytes();
         byte[] saltedPassword = new byte[base64EncodedPassword.length + saltBytes.length];
 
+        System.out.println(base64EncodedPassword);
+        System.out.println(salt);
         for (int i = 0; i < saltedPassword.length; i++)
         {
-            if (i < base64EncodedPassword.length)
+            if (i < saltBytes.length)
             {
                 saltedPassword[i] = saltBytes[i];
             } else
@@ -153,22 +155,50 @@ public class Main
     //By default the default password is blank, and the user MUST change this before entering in new accounts, so this should be fine
     private static void createPasswordFile()
     {
+        Console console = System.console();
+        Scanner scanner = new Scanner(System.in);
+
         try
         {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filePathForPassword, true));
 
-            byte[] password = Base64.getEncoder().encode(defaultPassword.getBytes());
+            System.out.println("Please create a master password");
+            char[] passwordToSet;
+
+            //Console is null when run from within intellij debugger. Because of this, default to scanner if it doesnt find the console
+            if (console == null)
+                passwordToSet = scanner.next().toCharArray();
+            else
+                passwordToSet = console.readPassword();
+
+            //Copy all of the bytes of characters into the byte array, act as a converter
+            byte[] byteArrayOfPassword = new byte[passwordToSet.length];
+            for (int j = 0; j < byteArrayOfPassword.length; j++)
+            {
+                byteArrayOfPassword[j] = (byte) passwordToSet[j];
+            }
+
+            //Clear contents of password array.
+            Arrays.fill(passwordToSet, '0');
+
+
+            byte[] password = Base64.getEncoder().encode(byteArrayOfPassword);
 
             int salt = new SecureRandom().nextInt(256);
 
-            writer.write(salt);
-            writer.write(Arrays.toString(saltAndHashPassword(password, salt)));
+            writer.write(salt + "\n");
+            writer.write(saltAndHashPassword(password, salt).toString());
+
+            writer.close();
 
             //Delete the accounts file if it made it this far, because you can't access any of these accounts any more anyway
             Files.delete(Paths.get(filePathForAccounts));
+        } catch (NoSuchFileException e)
+        {
+            System.out.println("     Account file does not exist at this point yet");
         } catch (IOException e)
         {
-            e.printStackTrace();
+            System.out.println("Unexpected error occurred");
         }
     }
 
@@ -248,12 +278,7 @@ public class Main
      *
      * @return boolean for if the password matches
      */
-    //TODO write this, use the password input thing from console.
-    //it needs to hash and check and yeah man
-    //If the password is wrong it should loop and ask again. maybe 3 tries before closing program?
-    //Right now i have the logic set up in other methods saying that it should only continue if it returns true, but it is technically more secure if it crashes after a few wrong answers.
-    //Maybe make it self destruct and delete password file, making the accounts unrecoverable if there are too many guesses, to add to security? - I dont care, just an idea
-    //Who ever finishes this, its your call
+
     //I do know, that because of IO errors it does have to fail closed, not open so be aware of that vulnerability
     private static boolean confirmPassword(byte[] password) throws FileNotFoundException
     {
@@ -264,15 +289,16 @@ public class Main
             //This should only be thrown if this is called by the "logInAtProgramLaunch" as it should be impossible to pass that method without the file existing
 
             //Skip salt line of file.
-            reader.readLine();
+            int salt = Integer.parseInt(reader.readLine());
 
             //Pull hash off of the file to compare.
-            byte[] hashedVersionOfSavedPasswordOnFile = Base64.getEncoder().encode(reader.readLine().getBytes());
+            String hashedVersionOfSavedPasswordOnFile = reader.readLine();
 
 
-            byte[] enteredPassword = Base64.getEncoder().encode(password);
+            byte[] passwordToTest = saltAndHashPassword(Base64.getEncoder().encode(password), salt);
 
-            if (Arrays.equals(enteredPassword, hashedVersionOfSavedPasswordOnFile))
+
+            if (passwordToTest.toString().equals(hashedVersionOfSavedPasswordOnFile))
             {
                 System.out.println("Password matches, continuing");
                 return true;
@@ -283,8 +309,6 @@ public class Main
         }
         return false;
     }
-
-    //TODO After getting this return value make sure this array is cleared.
 
     /**
      * Prompts user to input a password. This will be matched to the password that is salted and hashed already saved on the file system.
@@ -524,17 +548,23 @@ public class Main
      * On program launch, this method should initially confirm the password to launch the program
      * If a password does not yet exist, set a password for
      */
-    private static void logInAtProgramLaunch()
+    private static boolean logInAtProgramLaunch()
     {
         try
         {
-            confirmPassword();
+            if (confirmPassword() != null)
+            {
+                return true;
+            }
 
         } catch (FileNotFoundException e) //Password file not found
         {
             System.out.println("You must set a password");
             createPasswordFile();
+            return true;
         }
+
+        return false;
     }
 
     /**
@@ -623,7 +653,7 @@ public class Main
             Files.write(Paths.get(filePathForPassword), Base64.getEncoder().encode(Integer.toString(salt).getBytes()));
 
             BufferedWriter writer = new BufferedWriter(new FileWriter(filePathForPassword, true));
-            writer.write(Arrays.toString(hashedVersionOfNewPassword));
+            writer.write('\n' + Arrays.toString(hashedVersionOfNewPassword));
 
             //Update all accounts with the new password.
             updateAllAccountsForNewPassword(newPasswordByteArray, oldPasswordByteArray);
@@ -845,10 +875,11 @@ public class Main
     // Initialize and launch program
     public static void main(String[] args)
     {
-        initPrivateKeyAndIV();
+//        initPrivateKeyAndIV();
 
-        logInAtProgramLaunch();
+        if (!logInAtProgramLaunch())
+            return;
 
-        mainLoop();
+//        mainLoop();
     }
 }
